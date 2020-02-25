6066c5235ab137963fda211f6e4c100ec2ccab57
miguelsalva <miguelsalva@gmail.com>
2020-02-25 16:36:27 +0100

    Change verification

--- CursoPythondesde0/practica_comprueba_formato_email.py
+++ CursoPythondesde0/practica_comprueba_formato_email.py
@@ -1,12 +1,27 @@
 
-contador = 0
+#contador = 0
+#miEmail=input("Introduce tu dirección de email: ")
+
+#for i in miEmail:
+#    if (i=="@" or i=="."):
+#        contador = contador + 1
+
+#if contador == 2:
+#    print("El email es correcto")
+#else:
+#    print("El email no es correcto")
+
+tiene_arroba = False
+tiene_punto = False
 miEmail=input("Introduce tu dirección de email: ")
 
 for i in miEmail:
-    if (i=="@" or i=="."):
-        contador = contador + 1
+    if (i == "@"): 
+        tiene_arroba = True
+    elif (i == "."):
+        tiene_punto = True
 
-if contador == 2:
+if (tiene_arroba and tiene_punto):
     print("El email es correcto")
 else:
     print("El email no es correcto")
