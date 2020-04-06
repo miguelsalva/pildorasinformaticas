db15aa1c1abc90db86c8806941ae9181e6d855f8
Miguel Salvá <miguelsalva@gmail.com>
2020-04-06 10:24:10 +0200

    Update practica_comprueba_formato_email.py

--- CursoPythondesde0/practica_comprueba_formato_email.py
+++ CursoPythondesde0/practica_comprueba_formato_email.py
@@ -1,3 +1,10 @@
+# Ejercicio 3:
+# Crea un programa que pida introducir una dirección de email por teclado. El programa
+# debe imprimir en consola si la dirección de email es correcta o no en función de si esta
+# tiene el símbolo ‘@’. Si tiene una ‘@’ la dirección será correcta. Si tiene más de una o
+# ninguna ‘@’ la dirección será errónea. Si la ‘@’ está al comienzo de la dirección de email
+# o al final, la dirección también será errónea
+
 
 #contador = 0
 #miEmail=input("Introduce tu dirección de email: ")
